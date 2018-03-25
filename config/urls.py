874faed358f3fa8214d6570433b39d9d16b0b446
Roman Afanaskin <r.afanaskin@yandex.ru>
2018-03-25 13:49:03 +0300

    add home page

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView, TemplateView
+from django.conf import settings
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    path('favicon.ico', RedirectView.as_view(url=f'{settings.STATIC_URL}favicon.ico', permanent=True)),
     path('admin/', admin.site.urls),
 ]
